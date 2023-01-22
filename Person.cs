2a694f0ecd46a6c7728cd063171d8b1d26bbc8ac
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-01-21 18:44:06 -0700

    added written files

---
+++ Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211DPersonLab
+{
+    internal class Person
+    {
+        //fields
+        private int     id;
+        private string  firstName;
+        private string  lastName;
+        private int     age;
+        private string  favoriteColor;
+        private bool    isWorking;
+
+        //properties
+        public int      Id              {get => id; set => id = value; }
+        public string   FirstName       { get => firstName; set => firstName = value; }
+        public string   LastName        { get => lastName; set => lastName = value; }
+        public int      Age             { get => age; set => age = value; }
+        public string   FavoriteColor   { get => favoriteColor; set => favoriteColor = value; }
+        public bool     IsWorking       { get => isWorking; set => isWorking = value; }
+
+        public Person(int id, string firstName, string lastName, int age, string favoriteColor, bool isWorking)
+        {
+            this.id =           id;
+            this.firstName =    firstName;
+            this.lastName =     lastName;
+            Age =              age;
+            FavoriteColor =     favoriteColor;
+            IsWorking =         isWorking;
+        }
+
+        public override string ToString()
+        {
+            string formatted = "";
+
+            formatted += "ID: " + id + "\n";
+            formatted += "First name: " + firstName + "\n";
+            formatted += "Last name: " + lastName + "\n";
+            formatted += "FavoriteColor: " + favoriteColor + "\n";
+            formatted += "Age: " + age + "\n";
+            formatted += "IsWorking: " + isWorking + "\n";
+
+            return formatted;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211DPersonLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1, "Ian", "Brooks", 30, "Red", true);
+            Person person2 = new Person(2, "Gina", "James", 18, "Green", false);
+            Person person3 = new Person(3, "Mike", "Briscoe", 45, "Blue", true);
+            Person person4 = new Person(4, "Mary", "beals", 28, "Yellow", true);
+
+            Console.WriteLine(person1.ToString());
+            Console.WriteLine(person2.ToString());
+            Console.WriteLine(person3.ToString());
+            Console.WriteLine(person4.ToString());
+            Console.ReadLine();
+        }
+    }
+}
